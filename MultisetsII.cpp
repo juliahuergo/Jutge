2b0eade3626e3747c75b1d8f5f80d828e5196e0d
Julia Huergo Muñoz <95702056+juliahuergo@users.noreply.github.com>
2025-12-27 19:59:54 +0100

    Add files via upload

---
+++ MultisetsII.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void backtrack(int idx, int n, vector<int>& multiset, int x, int y) {
+	if (idx == n + 1) {
+		cout << "{";
+		for (int i = 0; i < multiset.size(); i++) {
+			cout << multiset[i];
+			if (i != multiset.size() - 1)
+				cout << ",";
+		}
+		cout << "}\n";
+		return;
+	}
+
+	for (int i = x; i <= y; i++) {
+		for (int j = 0; j < i; j++)
+			multiset.push_back(idx);
+		backtrack(idx + 1, n, multiset, x, y);
+		for (int j = 0; j < i; j++)
+			multiset.pop_back();
+	}
+}
+
+bool casoDePrueba() {
+	int n, x, y;
+	cin >> n;
+
+	if (!cin)
+		return false;
+
+	cin >> x >> y;
+
+	vector<int> multiset;
+	backtrack(1, n, multiset, x, y);
+
+	return true;
+}
+
+int main() {
+	while (casoDePrueba());
+	return 0;
+}
